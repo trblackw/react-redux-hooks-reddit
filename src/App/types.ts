87e25cbb3da949d887ebcd884cee18f95ed43ef2
Tucker <trblackw@ncsu.edu>
2019-08-11 22:51:39 -0400

    Add enum for hardcoded subreddits

---
+++ src/App/types.ts
@@ -0,0 +1,11 @@
+export enum SubReddit {
+   ReactJS = 'reactjs',
+   Angular = 'angular',
+   WebDev = 'webdev',
+   LearnJS = 'learnjavascript',
+   WebDesign = 'webdesign',
+   SoftwareGore = 'softwaregore',
+   ProgrammerHumor = 'programmerhumor',
+   Node = 'node',
+   TypeScript = 'typescript'
+}
