49150f7408560c2d853278e8c00bc49e736a4704
Tucker <trblackw@ncsu.edu>
2019-08-12 20:34:09 -0400

    Call init function on component mount

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createElement } from 'react';
 import { render } from 'react-dom'
 import App from './App/App';
+import { init } from './App/Api';
 
-render(createElement(App), document.querySelector('main'))
+render(createElement(App), document.querySelector('main'), () => init())
