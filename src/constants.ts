704d8234269535d4d88f74d048d316829165ec60
Tucker <trblackw@ncsu.edu>
2019-08-14 21:47:29 -0400

    Add action constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 export const FETCH_POSTS: string = 'FETCH_POSTS';
 export const FETCH_SINGLE_POST: string = 'FETCH_SINGLE_POST';
+export const TOGGLE_THEME: string = 'TOGGLE_THEME';
+export const SET_SUBREDDIT: string = 'SET_SUBREDDIT';
+export const CLEAR_SELECTED_POST: string = 'CLEAR_SELECTED_POST';
