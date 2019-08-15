49a658a601b683c2b2595cc4d76cda7e624fa8a6
Tucker <trblackw@ncsu.edu>
2019-08-15 18:01:46 -0400

    Add util func to extract hostname from links

--- src/App/utils.ts
+++ src/App/utils.ts
@@ -10,3 +10,12 @@ export const formatTimeStamp = (timeStamp: number): string => {
    const time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
    return time;
 };
+
+export const extractHostName = (url: string): string => {
+   let hostname = url.indexOf('//') > -1 ? url.split('/')[2] : url.split('/')[0];
+   //find & remove port number
+   hostname = hostname.split(':')[0];
+   //find & remove "?"
+   hostname = hostname.split('?')[0];
+   return hostname;
+};
