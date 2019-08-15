2d3947fa684648945242aaf0a3aac2b4e210b343
Tucker <trblackw@ncsu.edu>
2019-08-14 21:48:48 -0400

    Set up app theme

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,12 +2,36 @@ import React from 'react';
 import { Router } from '@reach/router';
 import Post from './components/Post';
 import Posts from './components/Posts';
+import { Themes } from './state';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import Header from './components/Header';
 
-const App = () => (
-   <Router>
-      <Posts path='/' />
-      <Post path='/:id' />
-   </Router>
-);
+const App = () => {
+   //@ts-ignore
+   const theme = useSelector(({ theme }: Themes) => theme);
+   return (
+      <AppContainer theme={theme}>
+         <Header />
+         <Router>
+            <Posts path='/' />
+            <Post path='/:id' />
+         </Router>
+      </AppContainer>
+   );
+};
 
+const AppContainer = styled.div`
+   max-width: 1100px;
+   margin: 0 auto;
+   background-color: ${({ theme }) => theme === Themes.light ? '#fff' : '#222'};
+   transition: background-color 0.7s ease;
+   color: ${({ theme }) => theme === Themes.light ? '#222' : '#eee'};
+   transition: color 0.7s ease;
+
+   @media screen and (max-width: 375px) {
+      margin: unset;
+      width: 100%;
+   }
+`;
 export default App;
