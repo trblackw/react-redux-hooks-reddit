d4e00e85a7e1eadf4ec2a08318adb4c025211765
Tucker <trblackw@ncsu.edu>
2019-08-15 21:27:40 -0400

    Adjust ViewContainer styles

---
+++ src/App/components/SharedStyledComponents.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ViewContainer = styled.div`
+   min-height: 100vh;
+   height: 100%;
+   box-sizing: border-box;
+   padding: 0 20px;
+   margin: 1em auto;
+`;
