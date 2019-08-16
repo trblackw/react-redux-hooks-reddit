7f5fe027beda46677dc786583c122772dace57a7
Tucker <trblackw@ncsu.edu>
2019-08-15 21:05:12 -0400

    Add responsive search bar

--- src/App/components/Header.tsx
+++ src/App/components/Header.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { TOGGLE_THEME, SET_SUBREDDIT } from '../../constants';
 import ThemeToggler from './ThemeToggler';
 import styled from 'styled-components';
+import { SubReddit } from '../types';
+import useViewport from '../hooks/useViewport';
 
 const Header = () => {
    const dispatch = useDispatch();
+   const selectedSubReddit = useSelector(({ selectedSubReddit }: { selectedSubReddit: SubReddit }) => selectedSubReddit);
+   const [{ width: windowWidth }] = useViewport();
    return (
-      <Nav>
-         <ButtonWrapper>
-            <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'all' })}>All</NavButton>
-            <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'popular' })}>Popular</NavButton>
-            <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'original' })}>Original Content</NavButton>
-            <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'original' })}>Favorites</NavButton>
-         </ButtonWrapper>
-            <SearchInput />
-         <ThemeToggler onClick={() => dispatch({ type: TOGGLE_THEME })} />
-      </Nav>
+      <Fragment>
+         <Nav>
+            <ButtonWrapper>
+               <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'all' })}>All</NavButton>
+               <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'popular' })}>Popular</NavButton>
+               <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'original' })}>Original</NavButton>
+               <NavButton onClick={() => dispatch({ type: SET_SUBREDDIT, payload: 'original' })}>Favorites</NavButton>
+               {windowWidth >= 948 && (
+                  <FlexWrapper>
+                     <SearchLabel>{`${selectedSubReddit} / `}</SearchLabel>
+                     <SearchInput />
+                  </FlexWrapper>
+               )}
+            </ButtonWrapper>
+            <ThemeToggler onClick={() => dispatch({ type: TOGGLE_THEME })} />
+         </Nav>
+         {windowWidth < 947 && (
+            <FlexWrapper>
+               <SearchLabel>{`${selectedSubReddit}/ `}</SearchLabel>
+               <SearchInput />
+            </FlexWrapper>
+         )}
+      </Fragment>
    );
 };
 
@@ -36,6 +53,7 @@ const ButtonWrapper = styled.div`
    align-self: flex-start;
    justify-content: flex-start;
    margin-right: auto;
+   width: 100%;
    > * {
       margin: 0 10px;
    }
@@ -48,6 +66,7 @@ const NavButton = styled.button.attrs({ type: 'button' })`
    border-radius: 4px;
    font-size: 1em;
    color: #eee;
+   align-self: center;
 
    &:hover {
       transform: scale(1.1);
@@ -57,12 +76,30 @@ const NavButton = styled.button.attrs({ type: 'button' })`
    }
 `;
 
-const SearchInput = styled.input.attrs({type: 'text', placeholder: 'search' })`
-   padding: 7px;
+const FlexWrapper = styled.div`
+   display: flex;
+   justify-content: flex-start;
+   width: 60%;
+   flex-direction: row;
+
+   @media screen and (max-width: 947px) {
+      margin-left: 15px;
+      width: 90%;
+   }
+`;
+
+const SearchLabel = styled.label`
+   display: inline-block;
+   margin-right: 4px;
+`;
+
+const SearchInput = styled.input.attrs({ type: 'text' })`
+   padding: 4px;
    border-radius: 20px;
    text-indent: 12px;
-   width: 40%;
+   width: 100%;
+   max-width: 400px;
+
    font-size: 0.8em;
-   margin: 0 auto;
    border: 1px thin gray;
-`
+`;
